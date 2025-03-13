ec2ce3ae3d3d5c1d723df51e8c8f202809b30780
Jenga Jones <rocknroll2793@gmail.com>
2025-03-13 18:36:21 -0500

    Added resume to portfolio to be viewed in browser

--- src/Components/BodyHeader.js
+++ src/Components/BodyHeader.js
@@ -1,10 +1,16 @@
 import './BodyHeader.css'
 
 function BodyHeader() {
-
     return (
         <header className="App-header">
         <div className="header-icons">
+          <a
+            href={process.env.PUBLIC_URL + "/TylerResume.pdf"} 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            <i className="fas fa-file-alt"></i>
+          </a>
           <a href="https://www.linkedin.com/in/tyler-will-57029916a/">
             <i className="fab fa-linkedin"></i>
           </a>
